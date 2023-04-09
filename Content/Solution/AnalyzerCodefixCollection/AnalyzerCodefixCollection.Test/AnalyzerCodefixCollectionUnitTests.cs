d53684cf11961e79c307330deb7f511efd2ddfb1
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2023-04-09 16:44:17 +0200

    Initial commit

---
+++ Content/Solution/AnalyzerCodefixCollection/AnalyzerCodefixCollection.Test/AnalyzerCodefixCollectionUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Verify = AnalyzerCodefixCollection.Test.Utilities.CSharpVerifier<AnalyzerCodefixCollection.AnalyzerCodefixCollectionAnalyzer>;
+    
+namespace AnalyzerCodefixCollection.Test
+{
+    
+    public class AnalyzerCodefixCollectionUnitTest
+    {
+        //No diagnostics expected to show up
+        [Fact]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await Verify.VerifyAnalyzerAsyncV2(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [Fact]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class [|TypeName|]
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = Verify.Diagnostic("AnalyzerCodefixCollection");
+            await Verify.VerifyCodeFixAsyncV2(test, fixtest);
+        }
+    }
+}
---
+++ Content/Solution/AnalyzerCodefixCollection/AnalyzerCodefixCollection/AnalyzerCodefixCollectionAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AnalyzerCodefixCollection
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AnalyzerCodefixCollectionAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "$AnalyzerId$";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static string Title = "$AnalyzerTitle$";
+
+        private const string MessageFormat = "$AnalyzerMessageFormat$";
+        private const string Description = "$AnalyzerDescription$";
+        private const string Category = "$AnalyzerCategory$";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
